fe89e6841356cf7fd15ad05015ac2baf073081cc
lathaniel <lathan.adam@gmail.com>
2022-10-11 14:13:25 -0600

    create directory if not exists

--- create_kaggle_metadata.py
+++ create_kaggle_metadata.py
@@ -3,14 +3,24 @@ import json
 
 def main():
     if not os.path.exists('.kaggleconfig'):
-        raise Exception('Must supply a .kaggleconfig file in the root directory')
+        raise Exception(
+            'Must supply a .kaggleconfig file in the root directory'
+        )
     
     with open('.kaggleconfig', 'r') as f:
         # Read in kaggle config file
         data = json.loads(f.read())
 
+        os.makedirs(
+            os.path.dirname(data["kaggle-data-directory"]),
+            exist_ok=True
+        )
+
         # Write the dataset-metadata file
-        with open(data["kaggle-data-directory"] + '/dataset-metadata.json', 'w+') as w:
+        with open(
+            data["kaggle-data-directory"] + '/dataset-metadata.json',
+            'w+'
+        ) as w:
             w.write(json.dumps(data["dataset-metadata"], indent=4))
         
         # Write the dataset-metadata file
