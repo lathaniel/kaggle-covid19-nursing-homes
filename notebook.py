3e53f17428d4253ad0b2bfc38f625f2e51e29218
lathaniel <lathan.adam@gmail.com>
2022-10-11 11:03:00 -0600

    Added data source to notebook config for Kaggle

--- notebook.py
+++ notebook.py
@@ -12,9 +12,20 @@
 #     name: kaggle-cms
 # ---
 
+# # Initial Setup
+#
+# I often prefer to run my notebooks outside of Kaggle.
+# As a result, I specify at the top whether I am running the notebook locally or on Kaggle
+
+# + vscode={"languageId": "python"}
+localEnv = True
+
+# + vscode={"languageId": "python"}
 # imports and whatnot
 import pandas as pd
 
+# + vscode={"languageId": "python"}
 # Verify we can read dataset
-df = pd.read_csv('./kaggle-data/COVID_19_Nursing_Home_Data_09_25_2022.csv')
+kaggleDataDir = '/kaggle/input' if localEnv == False else './kaggle-data'
+df = pd.read_csv(f'{kaggleDataDir}/COVID_19_Nursing_Home_Data_09_25_2022.csv')
 df
